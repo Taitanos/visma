6273921d2d484679a34d5fe3f5533016c4d67386
Taitanos <hayaashi87@gmail.com>
2024-10-05 19:01:12 +0300

    a button has been added, when clicked, services are filtered

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import InfoBlock from "./components/info-block/InfoBlock";
 import InfoList from "./components/cards/list/InfoList";
 import MoreButton from "./components/buttons/more/MoreButton";
 import LinksList from "./components/links/LinksList";
+import {useState} from "react";
 
 const services = [
     {type: "услуги", description: "H.Lorem ipsum dolor sit amet."},
@@ -45,7 +46,25 @@ const grid = {
     gridGap: "20px"
 }
 
+
+const buttonContainer = {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '20px 0'
+}
+
 function App() {
+
+    const [dataServices, setDataServices] = useState(services)
+
+    function handleSort() {
+        const copyData = [...services].sort((a, b) => {
+            if (a.description < b.description) return -1;
+            if (a.description > b.description) return 1;
+            return 0;
+        })
+        setDataServices(copyData)
+    }
 
     return (
         <div>
@@ -57,12 +76,15 @@ function App() {
                 <InfoList data={services.slice(0, 4)} gridStyle={grid}/>
             </Section>
             <hr/>
+            <div style={buttonContainer}>
+                <button onClick={handleSort}>Sort</button>
+            </div>
             <Section gridStyle={grid}>
                 <InfoBlock title={mainContentSection.title}
                            content={mainContentSection.content}>
                     <LinksList/>
                 </InfoBlock>
-                <InfoList data={services} gridStyle={grid}/>
+                <InfoList data={dataServices} gridStyle={grid}/>
             </Section>
         </div>
     );
